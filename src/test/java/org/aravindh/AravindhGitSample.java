71581f82dde3288b189e8ea82c984e8d7ce72dc2
AravindhAS3 <aravindhamalanathan@gmail.com>
2023-09-20 12:30:59 +0530

    master

---
+++ src/test/java/org/aravindh/AravindhGitSample.java
@@ -0,0 +1,22 @@
+package org.aravindh;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+public class AravindhGitSample {
+	public static void main(String[] args) {
+	//String Reversal and palindrome or non
+	String s="malayalam";
+	String rev= "";
+	for(int i =s.length()-1;i>=0; i--) {
+		char charAt = s.charAt(i);
+		rev=rev+charAt;
+	}
+System.out.println(rev);
+
+}}
+	
+	
+
